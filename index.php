bf5da71ca66534b1203adff9b460c8906e72ab61
francessco410 <francessco410@gmail.com>
2017-08-09 15:27:01 +0100

    Form v2

--- index.php
+++ index.php
@@ -9,98 +9,144 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title></title>
     </head>
     <body>
-        <?php
-
-        ?>
-        <div class="wrapper" style="padding: 20rem; padding-top: 5rem;">
-          <form>
-            <div class="form-inline row">
+        <div class="wrapper" style="padding: 10rem">
+            <form>
                 <div class="form-group">
-                  <label for="example-text-input" class="col-2 col-form-label">Name</label>
-                  <div class="col-10">
-                    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
-                  </div>
+                  <label for="exampleInputEmail1">First name</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="First name">
                 </div>
                 <div class="form-group">
-                  <label for="example-search-input" class="col-2 col-form-label">Search</label>
-                  <div class="col-10">
-                    <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
-                  </div>
+                  <label for="exampleInputPassword1">Last name</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last name">
                 </div>
-            </div>
-              
-              
-            <div class="form-group row">
-              <label for="example-email-input" class="col-2 col-form-label">Email</label>
-              <div class="col-10">
-                <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-url-input" class="col-2 col-form-label">URL</label>
-              <div class="col-10">
-                <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-tel-input" class="col-2 col-form-label">Telephone</label>
-              <div class="col-10">
-                <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-password-input" class="col-2 col-form-label">Password</label>
-              <div class="col-10">
-                <input class="form-control" type="password" value="hunter2" id="example-password-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-number-input" class="col-2 col-form-label">Number</label>
-              <div class="col-10">
-                <input class="form-control" type="number" value="42" id="example-number-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-datetime-local-input" class="col-2 col-form-label">Date and time</label>
-              <div class="col-10">
-                <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-date-input" class="col-2 col-form-label">Date</label>
-              <div class="col-10">
-                <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-month-input" class="col-2 col-form-label">Month</label>
-              <div class="col-10">
-                <input class="form-control" type="month" value="2011-08" id="example-month-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-week-input" class="col-2 col-form-label">Week</label>
-              <div class="col-10">
-                <input class="form-control" type="week" value="2011-W33" id="example-week-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-time-input" class="col-2 col-form-label">Time</label>
-              <div class="col-10">
-                <input class="form-control" type="time" value="13:45:00" id="example-time-input">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="example-color-input" class="col-2 col-form-label">Color</label>
-              <div class="col-10">
-                <input class="form-control" type="color" value="#563d7c" id="example-color-input">
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Email</label>
+                  <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <fieldset class="form-group">
+                  <legend>Sex</legend>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                      Male
+                    </label>
+                  </div>
+                  <div class="form-check">
+                  <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+                      Female
+                    </label>
+                  </div>
+                </fieldset>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Country</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Study Course</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Home University</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1">
+                </div>
+                <div class="form-group ">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">When do you plan to arrive in Braganca?</label>
+                   <i class="fa fa-calendar"></i>
+                  <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+                </div>
+                <fieldset class="form-group">
+                  <legend>Type of room you want to reserve </legend>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                      Single
+                    </label>
+                  </div>
+                  <div class="form-check">
+                  <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+                      Double
+                    </label>
+                  </div>
+                </fieldset>
+                <fieldset class="form-group">
+                  <legend>Would you like to reserve utility kit?</legend>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                      Yes
+                    </label>
+                  </div>
+                  <div class="form-check">
+                  <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+                      No
+                    </label>
+                  </div>
+                </fieldset>
+                <fieldset class="form-group">
+                  <legend>Are you a smoker?</legend>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                      Yes
+                    </label>
+                  </div>
+                  <div class="form-check">
+                  <label class="form-check-label">
+                      <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+                      No
+                    </label>
+                  </div>
+                </fieldset>
+                    
+             
+                <div class="form-group">
+                  <label for="exampleTextarea">Example textarea</label>
+                  <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                  <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+                </div>
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+          </form>       
         </div>
     </body>
 </html>
+
+<!-- Extra JavaScript/CSS added manually in "Settings" tab -->
+<!-- Include jQuery -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+<script>
+	$(document).ready(function(){
+		var date_input=$('input[name="date"]'); //our date input has the name "date"
+		var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+		date_input.datepicker({
+			format: 'mm/dd/yyyy',
+			container: container,
+			todayHighlight: true,
+			autoclose: true,
+		})
+	})
+</script>
